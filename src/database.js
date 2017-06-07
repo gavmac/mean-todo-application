2b27f45793c264b721cd17c5650ff672d7cd8865
gavmac <gavinmacken87@gmail.com>
2017-06-07 13:03:34 -0400

    Read me

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_h8m062bg:282820gG@ds113282.mlab.com:13282/heroku_h8m062bg', function(err) {
+mongoose.connect('mongodb://gavtest:282820gavtest@ds113282.mlab.com:13282/heroku_h8m062bg', function(err) {
   if (err) {
     console.log('Failed connecting to MongoDB!');
   } else {
