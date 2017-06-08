0e2e36181422494a148a5f9cc1bcff97b3a49d3d
gavmac <gavinmacken87@gmail.com>
2017-06-08 12:19:02 -0400

    Read me

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -2,5 +2,34 @@
 
 var angular = require('angular');
 
-angular.module('todoListApp').controller('mainCtrl', require('./main'));
+angular.module('todoListApp').controller('mainCtrl', ['$scope', 'dataService', function($scope, dataService) {
+
+    dataService.getTodos(function(response){
+        var todos = response.data.todos;
+        $scope.todos =  todos;
+    });
+
+    $scope.addTodo = function() {
+        $scope.todos.unshift({name: "This is a new todo.",
+            completed: false});
+    };
+
+}]);
+
 angular.module('todoListApp').controller('todoCtrl', require('./todo'));
+
+function MainCtrl ($scope, dataService) {
+
+    dataService.getTodos(function(response){
+        var todos = response.data.todos;
+        $scope.todos =  todos;
+    });
+
+    $scope.addTodo = function() {
+        $scope.todos.unshift({name: "This is a new todo.",
+            completed: false});
+    };
+
+}
+
+module.exports = MainCtrl;
