3ac88ac992e8e1de81f8b21d6d7719c8b950dcca
gavmac <gavinmacken87@gmail.com>
2017-06-08 06:01:38 -0400

    Read me

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ var webpack = require('webpack'),
        path = require('path');
 
 module.exports = {
-    context: __dirname + '/app',
+    context: 'https://evening-plains-98647.herokuapp.com/app',
     entry: {
         app: './app.js',
         vendor: ['angular']
     },
     output: {
-        path: __dirname + '/public/scripts',
+        path: 'https://evening-plains-98647.herokuapp.com/public/scripts',
         filename: 'todo.bundle.js'
     },
     plugins: [
