cde6c57388f3526f3a3254d68e34d97aaadb04c4
gavmac <gavinmacken87@gmail.com>
2017-06-09 04:05:56 -0400

    Read me

--- app/scripts/services/index.js
+++ app/scripts/services/index.js
@@ -2,4 +2,4 @@
 
 var angular = require('angular');
 
-angular.module('todoListApp').service('dataService', require('./data'));
+angular.module('todoListApp').service('dataService', require('/data'));
